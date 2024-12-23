a33f6a8ba85b81917398531241f172fc3503354d
Mouhammed <mouhammedfakhro@outlook.com>
2024-12-23 15:02:35 +0100

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LandingPage from "./page";
+
+const Layout = () => {
+  return (
+    <html lang="en">
+      <body>
+        <LandingPage/>
+      </body>
+    </html>
+  );
+};
+
+export default Layout;

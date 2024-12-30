69dca87bf6ab1b2ad9bda58fd47cca3960e3ed62
Mouhammed <mouhammedfakhro@outlook.com>
2024-12-30 20:29:54 +0100

    updated routes

--- src/app/api/attractions/route.js
+++ src/app/api/attractions/route.js
@@ -1,7 +1,6 @@
-import { getID, getAttractions } from "../../../utils/fetchTripAdvisor.js";
+import { getAttractions } from "../../../utils/fetchTripAdvisor.js";
 
 export default async function GET(req, res) {
-  const city = req.query;
   const attractions = await getAttractions("malmö");
   res.status(200).json({ attractions });
   console.log(attractions);
--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,20 +1,27 @@
-<<<<<<< HEAD:src/app/components/Sidebar.jsx
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
 
 const Sidebar = () => {
-    const router = useRouter();
-    // buttons clicked
-    const aboutClicked = () => {router.push("/about");}
-    const eventsClicked = () => {router.push("/event");}
-    const attractionsClicked = () => {router.push("/attractions");}
-    const foodClicked = () => {router.push("/food");}
-    const hotelClicked = () => {router.push("/hotel");}
+  const router = useRouter();
+  // buttons clicked
+  const aboutClicked = () => {
+    router.push("/about");
+  };
+  const eventsClicked = () => {
+    router.push("/event");
+  };
+  const attractionsClicked = () => {
+    router.push("/attractions");
+  };
+  const foodClicked = () => {
+    router.push("/food");
+  };
+  const hotelClicked = () => {
+    router.push("/hotel");
+  };
 
-    function resetAllButtons() {
-
-    }
+  function resetAllButtons() {}
 
   return (
     <div className="w-[15%] text-sm space-y-1 text-black">
@@ -72,78 +79,3 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
-=======
-"use client";
-import React from "react";
-import { useRouter } from "next/navigation";
-
-const Sidebar = () => {
-    const router = useRouter();
-    // buttons clicked
-    const aboutClicked = () => {router.push("/about");}
-    const eventsClicked = () => {router.push("/event");}
-    const attractionsClicked = () => {router.push("/attractions");}
-    const foodClicked = () => {router.push("/food");}
-    const hotelClicked = () => {router.push("/hotel");}
-
-    function resetAllButtons() {
-
-    }
-
-  return (
-    <div className="w-[15%] text-sm space-y-1 text-black">
-      {/*about*/}
-      <button
-        id="about-button"
-        className="w-full bg-sky-100 rounded-md p-2 
-       hover:bg-blue-200 shadow-md shadow-black/20"
-        onClick={aboutClicked}
-      >
-        ABOUT
-      </button>
-
-      {/*events*/}
-      <button
-        id="events-button"
-        className="w-full bg-sky-100 rounded-md p-2
-     hover:bg-blue-200 shadow-md shadow-black/20"
-        onClick={eventsClicked}
-      >
-        EVENTS
-      </button>
-
-      {/*attractions*/}
-      <button
-        id="attractions-button"
-        className="w-full bg-sky-100 rounded-md p-2
-     hover:bg-blue-200 shadow-md shadow-black/20"
-        onClick={attractionsClicked}
-      >
-        ATTRACTIONS
-      </button>
-
-      {/*food*/}
-      <button
-        id="food-button"
-        className="w-full bg-sky-100 rounded-md p-2
-     hover:bg-blue-200 shadow-md shadow-black/20"
-        onClick={foodClicked}
-      >
-        FOOD
-      </button>
-
-      {/*hotel*/}
-      <button
-        id="hotel-button"
-        className="w-full bg-sky-100 rounded-md p-2
-     hover:bg-blue-200 shadow-md shadow-black/20"
-        onClick={hotelClicked}
-      >
-        HOTEL
-      </button>
-    </div>
-  );
-};
-
-export default Sidebar;
->>>>>>> 4df6aae488875dadff754dece05f1aaf1341b0d2:app/components/Sidebar.jsx

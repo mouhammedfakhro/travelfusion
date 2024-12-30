04c405d1e53c1f62bc8c73269c71f73c83a77e92
Mouhammed <mouhammedfakhro@outlook.com>
2024-12-30 20:18:43 +0100

    new page file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 import App from "./App";
-<<<<<<< HEAD:src/app/page.jsx
  
-=======
-import { GlobalProvider } from "./globalVariables";
-
->>>>>>> 4df6aae488875dadff754dece05f1aaf1341b0d2:app/page.jsx
 const Layout = () => {
   return (
     <html lang="en">
       <body>
-<<<<<<< HEAD:src/app/page.jsx
         <App />
-=======
-        <GlobalProvider>
-          <App/>
-        </GlobalProvider>
->>>>>>> 4df6aae488875dadff754dece05f1aaf1341b0d2:app/page.jsx
       </body>
     </html>
   );
